b0fbbf17833353b5571fa539ffdaeb434398135b
danielba777 <danielbauer230501@gmail.com>
2024-06-25 19:27:31 +0200

    Update ProductListScreen.jsx

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -50,7 +50,11 @@ const ProductListScreen = () => {
       }
     }
   }
-  
+
+  // Überprüfen, ob die Daten korrekt geladen wurden
+  if (isLoading) return <Loader />
+  if (error) return <Message variant='danger'>{error}</Message>
+
   return (
     <>
       <div className='flex flex-col gap-4'>
@@ -60,58 +64,58 @@ const ProductListScreen = () => {
       <div className='flex flex-col sm:flex-row w-full'>
         <div className='flex sm:flex-col gap-2 overflow-x-auto sm:w-1/5'>
 
-            <Link to='/admin/orderlist'>
+          <Link to='/admin/orderlist'>
             <button className='p-2 sm:rounded-md w-max hover:border-slate-400 sm:w-[220px] sm:h-[50px] sm:border sm:border-2 sm:border-slate-600'>
-                <div className='flex items-center gap-2'>
+              <div className='flex items-center gap-2'>
                 <div className='flex justify-center items-center w-max sm:w-[50px] text-xl sm:text-base'>
-                    <i className='fa-solid fa-cubes'></i>
+                  <i className='fa-solid fa-cubes'></i>
                 </div>
                 <div className='flex items-center justify-start text-xl sm:text-base w-max'>
-                    <p>Orders</p>
+                  <p>Orders</p>
                 </div>
-                </div>
+              </div>
             </button>
-            </Link>
+          </Link>
 
-            <Link to='/admin/userlist'>
-                <button className='p-2 sm:rounded-md w-max hover:border-slate-400 sm:w-[220px] sm:h-[50px] sm:border sm:border-2 sm:border-slate-600'>
-                <div className='flex items-center gap-2'>
-                    <div className='flex justify-center items-center w-max sm:w-[50px] text-xl sm:text-base'>
-                    <i className='fa-solid fa-users'></i>
-                    </div>
-                    <div className='flex items-center justify-start text-xl sm:text-base w-max'>
-                    <p>Users</p>
-                    </div>
-                </div>
-                </button>
-            </Link>
-
-            <button className='p-2 sm:rounded-md w-max sm:w-[220px] sm:h-[50px] sm:border sm:border-2 sm:border-slate-300 border-b-2 border-slate-400'>
-                <div className='flex items-center gap-2'>
+          <Link to='/admin/userlist'>
+            <button className='p-2 sm:rounded-md w-max hover:border-slate-400 sm:w-[220px] sm:h-[50px] sm:border sm:border-2 sm:border-slate-600'>
+              <div className='flex items-center gap-2'>
                 <div className='flex justify-center items-center w-max sm:w-[50px] text-xl sm:text-base'>
-                    <i className='fa-solid fa-tags'></i>
+                  <i className='fa-solid fa-users'></i>
                 </div>
                 <div className='flex items-center justify-start text-xl sm:text-base w-max'>
-                    <p>Products</p>
+                  <p>Users</p>
                 </div>
-                </div>
+              </div>
             </button>
+          </Link>
+
+          <button className='p-2 sm:rounded-md w-max sm:w-[220px] sm:h-[50px] sm:border sm:border-2 sm:border-slate-300 border-b-2 border-slate-400'>
+            <div className='flex items-center gap-2'>
+              <div className='flex justify-center items-center w-max sm:w-[50px] text-xl sm:text-base'>
+                <i className='fa-solid fa-tags'></i>
+              </div>
+              <div className='flex items-center justify-start text-xl sm:text-base w-max'>
+                <p>Products</p>
+              </div>
+            </div>
+          </button>
 
           <div className='hidden sm:flex items-center justify-center mt-4 sm:justify-start'>
             <button className='border border-2 border-slate-600 px-2 py-1 rounded-md w-32 hover:border-slate-400 min-w-[220px]' onClick={logoutHandler}>Logout</button>
           </div>
         </div>
         <div className='flex flex-col justify-center sm:w-4/5 gap-2'>
-        <div className='flex justify-between items-center'>
+          <div className='flex justify-between items-center'>
             <h2 className='text-xl font-semibold my-4'>All Products</h2>
             <button className='flex items-center gap-1 rounded-md w-max h-max py-1 px-2 border border-2 border-slate-600 hover:border-slate-400 hover:text-slate-400'
                     onClick={createProductHandler}>
-                <i className='fa-regular fa-square-plus'></i> 
-                <p>Create Product</p>
+              <i className='fa-regular fa-square-plus'></i> 
+              <p>Create Product</p>
             </button>
-        </div>
+          </div>
           
-          { isLoading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
+          { products ? (
             <div className='overflow-x-auto'>
               <table className='text-center w-full'>
                 <thead>
@@ -126,7 +130,7 @@ const ProductListScreen = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  { products.map((product) =>(
+                  {products.map((product) => (
                     <tr key={product._id} className='border-b-2 hover:bg-slate-200 cursor-pointer'>
                       <td>{product._id}</td>
                       <td>{product.productDisplayName}</td>
@@ -146,6 +150,8 @@ const ProductListScreen = () => {
                 </tbody>
               </table>
             </div>
+          ) : (
+            <Message variant='danger'>No Products Found</Message>
           )}
 
           <div className='h-32 sm:h-64' />
